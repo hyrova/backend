232643bbab7dac575105c87dc2f4d169f9c57ac2
TCM-dev <valentin.wojtasinski@gmail.com>
2021-02-17 23:28:44 +0100

    Seed admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory()->admin()->create([
+             'name' => 'admin',
+             'password' => 'admin',
+         ]);
     }
 }
